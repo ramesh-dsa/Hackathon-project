f23ffe1336933f22b39114d334220bed20f30c06
ramesh-dsa <ramesh.dsa.dev@gmail.com>
2026-09-15 22:36:28 +0530

    added clarity tracking

--- app/(public)/layout.js
+++ app/(public)/layout.js
@@ -1,16 +1,21 @@
+import ClarityInit from '@/components/Clarity';
 import { Navbar } from "../../components/layout/navbar";
 import { Footer } from "../../components/layout/footer";
 import { BackToTop } from "../../components/ui/back-to-top";
 
 export default function PublicLayout({ children }) {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
-      <Navbar />
-      <main className="flex-1">
-        {children}
-      </main>
-      <Footer />
-      <BackToTop />
-    </div>
+    <>
+      <ClarityInit />   {/* 👈 ADDED THIS LINE */}
+
+      <div className="flex min-h-screen flex-col bg-background">
+        <Navbar />
+        <main className="flex-1">
+          {children}
+        </main>
+        <Footer />
+        <BackToTop />
+      </div>
+    </>
   );
 }
